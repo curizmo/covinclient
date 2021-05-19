bba5cc917a12c13732db030222c2920d326e0b80
Anastasiia Shapovalova <nastikue@gmail.com>
2021-05-19 22:21:20 +0300

    update NonPhysician according to the design

--- src/pages/NonPhysician.js
+++ src/pages/NonPhysician.js
@@ -1,14 +1,56 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import styled from 'styled-components';
+
 import { DashboardLayout } from 'components/common/Layout';
+import { useAuthProvider } from 'hooks/useAuthProvider';
+import accessDenied from 'assets/images/access-denied.svg';
+
+const Wrapper = styled.div`
+  min-height: 80vh;
+  padding: 0 2rem;
+`;
+
+const Image = styled.img`
+  margin-bottom: 1.5rem;
+  max-width: 400px;
+`;
+
+const Header = styled.h1`
+  font-weight: 700;
+`;
+
+const Title = styled.h4`
+  font-weight: 700;
+`;
+
+const LinkButton = styled.button`
+  font-weight: 700;
+  text-decoration: underline;
+  color: #1f3259;
+`;
 
 const NonPhysician = () => {
+  const { onSignOut } = useAuthProvider();
+
   return (
     <DashboardLayout className="w-full">
-      <p>You do not have rights to access the application.</p>
-      <p>
-        Please contact the administrator or login with an active physician
-        account.
-      </p>
+      <Wrapper className="d-flex justify-content-center align-items-center flex-column">
+        <Image src={accessDenied} alt="access denied" />
+        <Header>403</Header>
+        <Title>Access Denied</Title>
+        <p>You do not have rights to access the application.</p>
+        <p className="mb-5">
+          Please contact the administrator or{' '}
+          <LinkButton onClick={onSignOut} className="transparent-button p-0">
+            login
+          </LinkButton>{' '}
+          with and active physician account.
+        </p>
+        <Button className="btn-covin" onClick={onSignOut}>
+          GO BACK
+        </Button>
+      </Wrapper>
     </DashboardLayout>
   );
 };
