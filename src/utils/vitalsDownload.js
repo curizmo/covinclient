e54a3189b3cf43a16e5916d212227e79d035bc4c
Shreets <shreeti.019@gmail.com>
2021-06-01 18:09:26 +0545

    refactor style for  excel sheet

--- src/utils/vitalsDownload.js
+++ src/utils/vitalsDownload.js
@@ -2,6 +2,24 @@ import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx-js-style';
 import { excelTitleCell } from '../constants/index';
 
+const Headerstyle = {
+  font: { sz: 12, bold: true, color: '#FF00FF' },
+  fill: {
+    fgColor: {
+      theme: 8,
+      tint: 0.3999755851924192,
+      rgb: 'FBE5D6',
+    },
+  },
+  wch: 16,
+  border: {
+    top: { style: 'thin', color: '#000000' },
+    bottom: { style: 'thin', color: '#000000' },
+    right: { style: 'thin', color: '#000000' },
+    left: { style: 'thin', color: '#000000' },
+  },
+};
+
 export const exportToCSV = (csvData) => {
   const identifier = new Date().getTime();
   const fileName = `${identifier}`;
@@ -10,22 +28,7 @@ export const exportToCSV = (csvData) => {
   const fileExtension = '.xlsx';
   const ws = XLSX.utils.json_to_sheet(csvData);
   excelTitleCell.map((cell) => {
-    ws[cell].s = {
-      font: { sz: 12, bold: true, color: '#FF00FF' },
-      fill: {
-        fgColor: {
-          theme: 8,
-          tint: 0.3999755851924192,
-          rgb: 'FBE5D6',
-        },
-      },
-      border: {
-        top: { style: 'thin', color: 'FBE5D6' },
-        bottom: { style: 'thin', color: 'FBE5D6' },
-        right: { style: 'thin', color: 'FBE5D6' },
-        left: { style: 'thin', color: 'FBE5D6' },
-      },
-    };
+    ws[cell].s = Headerstyle;
   });
   const wb = { Sheets: { vitals: ws }, SheetNames: ['vitals'] };
   const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
