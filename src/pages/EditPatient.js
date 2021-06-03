b40ec4af59a201a346b3cffd1c6cfb7d973cb024
Shreets <shreeti.019@gmail.com>
2021-06-03 13:09:53 +0545

    setup edit patient page

--- src/pages/EditPatient.js
+++ src/pages/EditPatient.js
@@ -3,10 +3,21 @@ import { useHistory, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { yupResolver } from '@hookform/resolvers/yup';
+import styled from 'styled-components';
+import {
+  Row,
+  Col,
+  Button,
+  Form,
+  Alert,
+  FormGroup,
+  Label,
+  CustomInput,
+} from 'reactstrap';
 
-import { Container, Row, Col, Button, Form, Alert } from 'reactstrap';
 import { DashboardLayout } from '../components/common/Layout';
 import { InputField } from '../components/common/InputField';
+import { LinkButton } from '../components/common/Button';
 
 import {
   getISODate,
@@ -14,14 +25,71 @@ import {
   getErrorMessage,
   arrayObjectFixer,
 } from '../utils';
-import * as patientService from '../services/patient';
-import { GENDER_OPTIONS } from '../constants';
-import { patientValidation } from 'validations';
+import csc from 'third-party/country-state-city';
 import { hideSpinner, showSpinner } from 'actions/spinner';
+import * as patientService from '../services/patient';
+import { GENDER_OPTIONS, INDIA_COUNTRY_CODE } from '../constants';
+import { patientValidation } from 'validations';
 import { routes } from 'routers';
+import time from 'assets/images/svg-icons/clock.svg';
+import { getDate } from 'global';
+import {
+  RadioLabel,
+  RadioInput,
+  OptionName,
+  DateAndTime,
+  DateAndTimeWrap,
+  InfoWrapper,
+  TimeImage,
+  ViewName,
+} from 'global/styles';
+
+const states = csc.getStatesOfCountry(INDIA_COUNTRY_CODE);
+
+const Headings = styled.section`
+  padding: 0 4em;
+  width: 100%;
+  @media (max-width: 768px) {
+    padding: 0;
+  }
+`;
+const Container = styled.section`
+  margin: 0 4em;
+  padding: 4em 10em;
+  background-color: #fff;
+  @media (max-width: 768px) {
+    padding: 2em;
+    margin: 0;
+  }
+  @media (max-width: 1024px) {
+    padding: 4em;
+  }
+`;
 
 const EditPatient = () => {
   const { patientId } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [serverError, setServerError] = useState('');
+  const [checkedGender] = useState('');
+  const [gender, setGender] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+  const [citiesList, setCitiesList] = useState([]);
+
+  const { register, handleSubmit, errors, getValues, setValue } = useForm({
+    resolver: yupResolver(patientValidation),
+    mode: 'onBlur',
+  });
+
+  const values = getValues();
+  const disabled = useMemo(() => {
+    return !(
+      values.firstName &&
+      values.phone &&
+      Object.keys(errors)?.length < 1
+    );
+  }, [errors, values]);
 
   const getPatient = async (patientId) => {
     try {
@@ -44,37 +112,36 @@ const EditPatient = () => {
       // TODO: Handle error
     }
   };
-  const history = useHistory();
-
-  const { register, handleSubmit, errors, getValues, setValue } = useForm({
-    resolver: yupResolver(patientValidation),
-    mode: 'onBlur',
-  });
 
   useEffect(() => {
     getPatient(patientId);
   }, [patientId]);
-  const values = getValues();
-  const dispatch = useDispatch();
 
-  const [serverError, setServerError] = useState('');
-  const disabled = useMemo(() => {
-    if (!Object.keys(errors).length) {
-      return false;
-    }
-    return true;
-  }, [errors, values]);
+  useEffect(() => {
+    setCitiesList(
+      state
+        ? csc.getCitiesOfState(
+            INDIA_COUNTRY_CODE,
+            csc.getStateIsoCodeByName(state),
+          )
+        : [],
+    );
+    setCity('');
+  }, [state]);
 
-  const handleSave = async (patientInput) => {
+  const handleSave = async (patient) => {
     try {
       dispatch(showSpinner());
-
       await patientService.updatePatient(patientId, {
-        ...patientInput,
+        ...patient,
+        phone: patient.phone.replace(/\(/g, '').replace(/\)/g, ''),
+        state,
+        city,
+        gender,
         patientId,
       });
 
-      history.push(routes.patients.path);
+      history.push(routes.dashboard.path);
     } catch (err) {
       if (err.response.data) {
         setServerError(err.response.data.message);
@@ -86,98 +153,191 @@ const EditPatient = () => {
 
   return (
     <DashboardLayout>
+      <Headings>
+        <InfoWrapper>
+          <ViewName>Edit Patient</ViewName>
+          <DateAndTimeWrap>
+            <TimeImage src={time} />
+            <DateAndTime>{getDate()}</DateAndTime>
+          </DateAndTimeWrap>
+        </InfoWrapper>
+      </Headings>
       <Container>
-        <Row>
-          <Col md={{ size: 8, offset: 2 }}>
-            <h2 className="mb-5">Edit Patient</h2>
-
-            <Form onSubmit={handleSubmit(handleSave)}>
+        <Form onSubmit={handleSubmit(handleSave)}>
+          <Row>
+            <Col md={{ size: 6 }}>
               <InputField
-                title="First Name:"
+                title="Cell Phone"
+                name="phone"
+                required
+                error={getErrorMessage(errors, 'phone')}
+                innerRef={register}
+                defaultValue="+91-"
+              />
+            </Col>
+            <Col md={{ size: 6 }}>
+              <InputField
+                title="Patient Email"
+                name="email"
+                type="email"
+                innerRef={register}
+                placeholder="Enter Patient Email ID"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{ size: 6 }}>
+              <InputField
+                title="First Name"
                 name="firstName"
                 required
                 innerRef={register}
                 error={getErrorMessage(errors, 'firstName')}
+                placeholder="Enter Patient Full Name"
               />
-
+            </Col>
+            <Col md={{ size: 6 }}>
               <InputField
-                title="Last Name:"
+                title="Last Name"
                 name="lastName"
-                required
                 innerRef={register}
-                error={getErrorMessage(errors, 'lastName')}
+                placeholder="Enter Patient Last Name (Optional)"
               />
-
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{ size: 3 }}>
+              <FormGroup check row className="mx-0 pl-0 form-group">
+                <Label>Gender</Label>
+                <div className="d-flex mt-2">
+                  {GENDER_OPTIONS.map(({ label, value }) => (
+                    <RadioLabel
+                      htmlFor={value}
+                      key={value}
+                      onClick={() => setGender(value)}>
+                      <RadioInput
+                        type="radio"
+                        name="gender"
+                        value={value}
+                        id={value}
+                      />
+                      <OptionName checked={checkedGender === value}>
+                        {label}
+                      </OptionName>
+                    </RadioLabel>
+                  ))}
+                </div>
+              </FormGroup>
+            </Col>
+            <Col md={{ size: 3 }}>
               <InputField
-                name="gender"
-                innerRef={register}
-                title="Gender:"
-                type="select"
-                placeholder="Choose gender">
-                <option value="" disabled defaultValue hidden default>
-                  Please Choose...
-                </option>
-
-                {GENDER_OPTIONS.map(({ label, value }) => (
-                  <option key={label} value={value} className="p-2">
-                    {label}
-                  </option>
-                ))}
-              </InputField>
-
-              <InputField
-                title="DOB:"
+                title="Date of Birth"
                 name="birthDate"
                 innerRef={register}
                 type="date"
                 max={getISODate(currentDate())}
+                placeholder="dd/mm/yyyy"
               />
-
+            </Col>
+            <Col md={{ size: 3 }}>
               <InputField
-                title="Email:"
-                name="email"
-                type="email"
-                required
-                error={getErrorMessage(errors, 'email')}
+                title="Height"
+                name="height"
                 innerRef={register}
+                error={getErrorMessage(errors, 'height')}
+                placeholder="Enter Height"
+                customClass="measurement ft"
               />
-
+            </Col>
+            <Col md={{ size: 3 }}>
               <InputField
-                title="Phone:"
-                name="phone"
-                error={getErrorMessage(errors, 'phone')}
+                title="Weight"
+                name="weight"
                 innerRef={register}
+                placeholder="Enter Weight"
+                customClass="measurement kg"
               />
-
+              <span />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
               <InputField
-                title="Address One:"
+                title="Address"
                 name="addressOne"
                 innerRef={register}
+                placeholder="Enter Address"
               />
-
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{ size: 6 }}>
               <InputField
-                title="Address Two:"
-                name="addressTwo"
+                tag={CustomInput}
+                onChange={(e) => setState(e.target.value)}
+                customClass="classic-dropdown"
+                defaultValue=""
+                title="State"
+                type="select"
+                name="state">
+                <option value="" disabled hidden default>
+                  {'Select State'}
+                </option>
+                {states.map(({ name }) => (
+                  <option key={name} value={name}>
+                    {name}
+                  </option>
+                ))}
+              </InputField>
+            </Col>
+            <Col md={{ size: 4 }}>
+              <InputField
+                disabled={citiesList.length < 1}
+                tag={CustomInput}
+                customClass="classic-dropdown"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                title="City"
+                type="select"
+                name="city">
+                <option value="" disabled hidden default>
+                  {'Select City'}
+                </option>
+                {citiesList.map(({ name }) => (
+                  <option key={name} value={name} onClick={() => setCity(name)}>
+                    {name}
+                  </option>
+                ))}
+              </InputField>
+            </Col>
+            <Col md={{ size: 2 }}>
+              <InputField
+                title="Postal Code"
+                name="zip"
                 innerRef={register}
+                placeholder="Postal Code"
               />
-
-              <InputField title="City:" name="city" innerRef={register} />
-
-              <InputField title="State:" name="state" innerRef={register} />
-
-              <InputField title="Zip:" name="zip" innerRef={register} />
-
-              <Col sm={{ size: 10, offset: 2 }}>
-                {serverError ? (
-                  <Alert color="danger">{serverError}</Alert>
-                ) : null}
-                <Button type="submit" disabled={disabled}>
-                  Update
+            </Col>
+          </Row>
+          <Row className="mt-3">
+            <Col>
+              {serverError ? <Alert color="danger">{serverError}</Alert> : null}
+              <div className="d-flex justify-content-end">
+                <LinkButton
+                  to={routes.dashboard.path}
+                  className="btn-cancel mr-2 cancel-add-patient">
+                  Cancel
+                </LinkButton>
+                <Button
+                  className="btn-covin add-patient"
+                  type="submit"
+                  disabled={disabled}>
+                  Save
                 </Button>
-              </Col>
-            </Form>
-          </Col>
-        </Row>
+              </div>
+            </Col>
+          </Row>
+        </Form>
       </Container>
     </DashboardLayout>
   );
