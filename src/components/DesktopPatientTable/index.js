2c631c0c0d2f2d7034d8df4b255f37a4856ddd28
Anastasiia Shapovalova <nastikue@gmail.com>
2021-06-07 22:50:53 +0300

    fix Dashboard scroll container height

--- src/components/DesktopPatientTable/index.js
+++ src/components/DesktopPatientTable/index.js
@@ -29,7 +29,7 @@ const Wrapper = styled.section`
   position: relative;
   padding: 0 4rem;
   width: 100%;
-  height: 100%;
+  height: calc(100% - 110px);
   overflow: ${(props) => (props?.isShowSpinner ? 'hidden' : 'scroll')};
 `;
 
